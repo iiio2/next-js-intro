e0b8955e967f97a04f5deb9de0155a25e92a8c2d
iiio2 <gomexxxx71@gmail.com>
2021-03-08 16:23:35 +0600

    added layout.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head';
-import Footer from '../comps/Footer';
-import Navbar from '../comps/Navbar';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h3>Hello, Next</h3>
-      <Footer />
     </div>
   );
 }
